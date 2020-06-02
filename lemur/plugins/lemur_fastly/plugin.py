d59a4d207f04b5393df30eb2c8e2bc90b14b00a7
alwaysjolley <chris@alwaysjolley.com>
2020-06-02 09:49:30 -0400

    rewrite of fastly to activate certificates when updated

--- lemur/plugins/lemur_fastly/plugin.py
+++ lemur/plugins/lemur_fastly/plugin.py
@@ -24,6 +24,7 @@ from lemur.plugins.bases import DestinationPlugin
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 
+
 class FastlyDestinationPlugin(DestinationPlugin):
     """Fastly Destination plugin for Lemur"""
 
@@ -34,21 +35,10 @@ class FastlyDestinationPlugin(DestinationPlugin):
     author = "Christopher Jolley"
     author_url = "https://github.com/Netflix/lemur"
 
-    options = [
-        {
-            "name": "fastlyFilter",
-            "type": "str",
-            "value": ".*",
-            "required": False,
-            "validation": ".*",
-            "helpMessage": "Valid regex filter",
-        },
-    ]
-
+    options = []
 
     def __init__(self, *args, **kwargs):
         super(FastlyDestinationPlugin, self).__init__(*args, **kwargs)
-
 
     def upload(self, name, body, private_key, cert_chain, options, **kwargs):
         """
@@ -58,28 +48,39 @@ class FastlyDestinationPlugin(DestinationPlugin):
         :param cert_chain:
         :return:
         """
-        key_id = None
+        priv_id = None
+        cert_id = None
+        unique = self.get_option("fastlyUnique", options)
         cname = common_name(parse_certificate(body))
         priv_keys = get_all_private_keys()
+        cert_keys = get_all_certificates()
         log_data = {
             "function": inspect.currentframe().f_code.co_name
         }
         for each in priv_keys:
             if each['name'] == cname:
-                key_id = each['id']
-                if each['sha1'] != get_public_key_sha1(private_key):
-                    cert_keys = get_all_certificates()
+                if each['sha1'] = get_public_key_sha1(private_key):
+                    priv_id = None
+                    break
+                else:
+                    priv_id = each['id']
                     for cert in cert_keys:
                         if cert['name'] == cname:
-                            delete_certificate(each['id'])
-                    delete_private_key(key_id)
-                    key_id = None
-        if not key_id:
-            post_private_key(private_key, name=cname)
-            post_certificate(body, cert_chain, name=cname)
+                            cert_id = cert['id']
+        if priv_id:
+            new_priv_id = post_private_key(private_key, name=cname)
+            new_cert_id = post_certificate(body, cert_chain, name=cname)
+            log_data["message"] = f"Certificate updated: ${cname}"
+            act_id = get_activation(cert_id)
+            if len(act_id) > 0 and new_cert_id:
+                for each in act_id:
+                    patch_activation(each, new_cert_id)
+                    log_data["message"] = f"Certificate updated: ${new_cert_id} activated: ${act_id}"
+            delete_certificate(cert_id)
+            delete_private_key(priv_id)
         else:
             log_data["message"] = f"Certificate up to data, no changes made"
-            current_app.logger.debug(log_data)
+        current_app.logger.debug(log_data)
 
 
 def get_public_key_sha1(private_key):
@@ -98,7 +99,7 @@ def get_public_key_sha1(private_key):
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     )
-    return hashlib.sha1(pub_key).hexdigest()
+    return hashlib.sha1(pub_key).hexdigest()  # nosec
 
 
 def get_all_private_keys():
@@ -131,7 +132,7 @@ def get_all_private_keys():
     except Exception as err:
         log_data['message'] = 'Failure to get all private keys'
         log_data['error'] = err
-        current_app.logger.debug(log_data)
+    current_app.logger.debug(log_data)
     return p_keys
 
 
@@ -164,7 +165,7 @@ def get_private_key(key_id):
     except Exception as err:
         log_data['message'] = f"Failure to get private key: {key_id}"
         log_data['error'] = err
-        current_app.logger.debug(log_data)
+    current_app.logger.debug(log_data)
     return jdata
 
 
@@ -191,18 +192,22 @@ def post_private_key(private_key, name=None):
             }
         }
     }
+    jdata = {}
+    priv_id = None
     log_data = {
         "function": inspect.currentframe().f_code.co_name
     }
     if name is not None:
         data['data']['attributes']['name'] = name
     try:
-        _post(path, data)
+        jdata = _post(path, data)
+        priv_id = jdata['data']['id']
         log_data['message'] = "private key upload: success"
     except Exception as err:
         log_data['message'] = "private key upload: failure"
         log_data['error'] = err
     current_app.logger.debug(log_data)
+    return priv_id
 
 
 def delete_private_key(key_id):
@@ -285,7 +290,7 @@ def get_certificate(key_id):
     }
     try:
         jdata = _get(path)
-        log_data['message'] = f"Fet certificate {key_id} success"
+        log_data['message'] = f"get certificate {key_id} success"
     except Exception as err:
         log_data['message'] = f"get certificate {key_id} failure"
         log_data['error'] = err
@@ -297,6 +302,7 @@ def post_certificate(cert, chain, name=None):
     """upload public certificate"""
     path = '/tls/certificates'
     full_cert = f"{cert}\n{chain}"
+    cert_id = None
     data = {
         "data": {
             "type": "tls_certificate",
@@ -311,12 +317,14 @@ def post_certificate(cert, chain, name=None):
     if name is not None:
         data['data']['attributes']['name'] = name
     try:
-        _post(path, data)
+        jdata = _post(path, data)
+        cert_id = jdata['data']['id']
         log_data['message'] = f"certificate upload {name} success"
     except Exception as err:
         log_data['message'] = f"certificate upload {name} failure"
         log_data['error'] = err
     current_app.logger.debug(log_data)
+    return cert_id
 
 
 def patch_certificate(cert, chain, key_id):
@@ -354,6 +362,69 @@ def delete_certificate(key_id):
         log_data['message'] = f"certificate delete {key_id} success"
     except Exception as err:
         log_data['message'] = f"certificate delete {key_id} failure"
+        log_data['error'] = err
+    current_app.logger.debug(log_data)
+
+
+def get_all_activations():
+    """get all the activations"""
+    path = '/tls/activations'
+    jdata = {}
+    log_data = {
+        "function": inspect.currentframe().f_code.co_name
+    }
+    try:
+        jdata = _get(path)
+        log_data['message'] = 'activation get success'
+    except Exception as err:
+        log_data['message'] = 'activation get failure'
+        log_data['error'] = err
+    current_app.logger.debug(log_data)
+    return jdata
+
+
+def get_activation(cert_id):
+    """get activation by certificate id"""
+    path = '/tls/activations'
+    jdata = {}
+    act_id = []
+    log_data = {
+        "function": inspect.currentframe().f_code.co_name
+    }
+    try:
+        jdata = _get(path)
+        for each in jdata['data']:
+            if each['relationships']['tls_certificate']['data']['id'] == cert_id:
+                act_id.append(each['id'])
+        log_data['message'] = 'activation get success'
+    except Exception as err:
+        log_data['message'] = 'activation get failure'
+        log_data['error'] = err
+    current_app.logger.debug(log_data)
+    return act_id
+
+
+def patch_activation(act_id, cert_id):
+    """update activation to use new certificate"""
+    path = f"/tls/activations/{act_id}"
+    log_data = {
+        "function": inspect.currentframe().f_code.co_name
+    }
+    data = {
+        "data": {
+            "type": "tls_activation",
+            "relationships": {
+                "tls_certificate": {
+                    "data": {"type": "tls_certificate", "id": cert_id}
+                }
+            }
+        }
+    }
+    try:
+        _patch(path, data)
+        log_data['message'] = f"activation patch {act_id} success"
+    except Exception as err:
+        log_data['message'] = f"activation pathc {act_id} failure"
         log_data['error'] = err
     current_app.logger.debug(log_data)
 
@@ -402,8 +473,8 @@ def _post(path, payload):
         headers=_generate_header(),
         verify=True
     )
-    print(resp.json)
     resp.raise_for_status()
+    return resp.json()
 
 
 def _patch(path, payload):
